b4cdb62d500379b8bffa1e73c246e843de721d4a
vonceee <alonarvoncedric@gmail.com>
2025-12-16 06:41:36 +0800

    refactor: footer responsive on mobile

--- src/app/components/main-footer/main-footer.ts
+++ src/app/components/main-footer/main-footer.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { faFacebookF, faInstagram, faYoutube, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+interface AccordionState {
+  [key: string]: boolean;
+}
+
 @Component({
   selector: 'main-footer',
-  imports: [FontAwesomeModule, RouterModule],
+  imports: [FontAwesomeModule, RouterModule, CommonModule],
   templateUrl: './main-footer.html',
   styleUrl: './main-footer.scss',
 })
@@ -14,4 +19,50 @@ export class MainFooter {
   faInstagram = faInstagram;
   faYoutube = faYoutube;
   faLinkedinIn = faLinkedinIn;
+
+  // Track which sections are expanded on mobile
+  expandedSections: AccordionState = {
+    solutions: false,
+    news: false,
+    trainings: false,
+    company: false,
+  };
+
+  isMobile = false;
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  toggleSection(section: string): void {
+    // Only toggle on mobile
+    if (this.isMobile) {
+      this.expandedSections[section] = !this.expandedSections[section];
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize(): void {
+    const wasMobile = this.isMobile;
+    this.isMobile = window.innerWidth < 768;
+
+    // If switching from mobile to desktop, reset all sections to expanded
+    if (wasMobile && !this.isMobile) {
+      Object.keys(this.expandedSections).forEach(key => {
+        this.expandedSections[key] = true;
+      });
+    }
+  }
+
+  isSectionExpanded(section: string): boolean {
+    // On desktop, all sections are always expanded
+    if (!this.isMobile) {
+      return true;
+    }
+    return this.expandedSections[section];
+  }
 }
