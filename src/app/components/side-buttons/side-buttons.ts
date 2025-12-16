9847e96cfed718d4bbdb15dc3d4e82aa5fd14f0b
vonceee <alonarvoncedric@gmail.com>
2025-12-17 05:53:51 +0800

    feat: Implement initial Angular application structure, introduce side-buttons component, and add home page styling.

---
+++ src/app/components/side-buttons/side-buttons.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'side-buttons',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './side-buttons.html',
+  styleUrl: './side-buttons.scss'
+})
+export class SideButtons {
+}
